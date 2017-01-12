9df71ca32ff6b58f5fed3342a675ae671e4b11ab
mep6 <mep6@ualberta.ca>
2017-01-11 18:11:14 -0700

    Changed script to pull the raw form of the script from git hub.

--- lab1.py
+++ lab1.py
@@ -3,7 +3,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/mep6/CMPUT404Lab1/master/lab1.py")
 
 print response.text
 print response.status_code
